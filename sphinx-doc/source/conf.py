0d2a7a5732e1fd864b034e2e587142af0ebff6c0
Masson, Lola <lola.masson@ifm.com>
2021-12-07 09:56:09 -0500

    Add custom stylesheet: change bg color to orange

--- sphinx-doc/source/conf.py
+++ sphinx-doc/source/conf.py
@@ -74,6 +74,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 
 myst_enable_extensions = [
     "colon_fence"
